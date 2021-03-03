0807cc700e0243c2dd16381697ee2859809afa74
Kustonm <65606861+Kustonm@users.noreply.github.com>
2021-03-03 09:04:36 +0100

    Create myzippack.c

---
+++ myzippack.c
@@ -0,0 +1,36 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/types.h>
+
+
+#include "s_Header.h"
+
+struct s_Header comp;
+
+int main(int argc , char *argv[]){
+
+  if(argc < 2){
+   fprintf(stderr,"Numero de argumentos erroneo \n");
+   exit(1);
+  }
+
+  if(open(argv[1]) == -1){
+   fprintf(stderr,"Fichero inexistente \n");
+   exit(2);
+  }
+
+  int size = lseek(argv[1], SEEK_END);
+  if(argc == 2){
+   int fich = open(argv[1],O_CREAT|O_WRONLY, 0600);
+   write(fich,fich,size);
+   close(fich);
+   return 0;
+  }else{
+   int fich1 = open(argv[1],O_CREAT|O_WRONLY, 0600);
+   int fich2 = open(argv[2],O_CREAT|O_WRONLY, 0600);
+   write(fich1,fich2,size);
+   close(fich1);
+   close(fich2);
+   return 0;
+ }
+}
