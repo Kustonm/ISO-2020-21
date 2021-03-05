b45fd7c7df1398e62ab30671c0431dc19f6435a1
Kustonm <65606861+Kustonm@users.noreply.github.com>
2021-03-05 10:59:17 +0100

    Update myzippack.c

--- Proyecto 1/myzippack.c
+++ Proyecto 1/myzippack.c
@@ -13,14 +13,11 @@ struct s_mypack_headers vector;
 struct s_Header comp;
 
 int main(int argc , char *argv[]){
-  
-  int fichE;
-
   if(argc < 3){
    fprintf(stderr,"Numero de argumentos erroneo \n");
    exit(1);
   }
-  if( open(argv[1],O_RDONLY ,0600) == -1){
+  if(open(argv[1],O_RDONLY ,0600) == -1){
    fprintf(stderr,"Fichero inexistente \n");
    exit(2);
   }
@@ -33,6 +30,7 @@ int main(int argc , char *argv[]){
   long size = lseek(argv[1], SEEK_END,0);
   char buff [512];
   if(open(argv[2],O_RDONLY,0600) == -1){
+    int fichE;
     fichE = open(argv[1],O_RDONLY ,0600);
     comp.FileInfo.Type = "\0";
     comp.FileInfo.Compress = "n";
@@ -45,7 +43,7 @@ int main(int argc , char *argv[]){
     }
    vector.vHead[0] = comp;
    int fichS = open(argv[2],O_CREAT|O_RDWR,0600);
-   while(read(fichE,buff,1) != -1){
+   while(read(fichE,buff,1) != 0){
     write(fichS,buff,1);
    }
    close(fichE);
@@ -54,7 +52,7 @@ int main(int argc , char *argv[]){
   }else{
    int fich1 = open(argv[1],O_RDONLY, 0600);
    int fich2 = open(argv[2],O_WRONLY, 0600);
-   while(read(fich1,buff,1) != -1){
+   while(read(fich1,buff,1) != 0){
     write(fich2,buff,1);
    }
    close(fich1);
