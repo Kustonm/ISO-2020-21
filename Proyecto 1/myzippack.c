9df4c9fa83f16f31e5e7e618ab4f027c97ec42be
Kustonm <65606861+Kustonm@users.noreply.github.com>
2021-03-05 09:55:28 +0100

    Update myzippack.c

--- Proyecto 1/myzippack.c
+++ Proyecto 1/myzippack.c
@@ -13,20 +13,21 @@ struct s_mypack_headers vector;
 struct s_Header comp;
 
 int main(int argc , char *argv[]){
-
-  int fdbook;
+  
+  int fichE;
 
   if(argc < 2){
    fprintf(stderr,"Numero de argumentos erroneo \n");
    exit(1);
   }
-  fdbook = open(argv[1],O_RDONLY ,0600);
-  if( fdbook == -1){
+  fichE = open(argv[1],O_RDONLY ,0600);
+  if( fichE == -1){
    fprintf(stderr,"Fichero inexistente \n");
    exit(2);
   }
 
   long size = lseek(argv[1], SEEK_END,0);
+  char buff [512];
   if(argc == 2){
     comp.FileInfo.Type = "\0";
     comp.FileInfo.Compress = "n";
@@ -37,14 +38,20 @@ int main(int argc , char *argv[]){
     {
       comp.FileInfo.DataFileName[i] = argv[1][i];
     }
-    vector.vHead[0] = comp;
-   write(fdbook,fdbook,size);
-   close(fdbook);
+   vector.vHead[0] = comp;
+   int fichS = open(argv[2],O_CREAT,0600);
+   while(read(fichE,buff,1) != -1){
+    write(fichS,buff,1);
+   }
+   close(fichE);
+   close(fichS);
    return 0;
   }else{
-   int fich1 = open(argv[1],O_CREAT|O_WRONLY, 0600);
-   int fich2 = open(argv[2],O_CREAT|O_WRONLY, 0600);
-   write(fich1,fich2,size);
+   int fich1 = open(argv[1],O_WRONLY, 0600);
+   int fich2 = open(argv[2],O_WRONLY, 0600);
+   while(read(fich1,buff,1) != -1){
+    write(fich2,buff,1);
+   }
    close(fich1);
    close(fich2);
    return 0;
