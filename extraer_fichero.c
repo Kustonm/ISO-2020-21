11e0334cd5564cbee4beb2d5f7c0ce46193a4c38
JonyyyV <jontxuuv@gmail.com>
2021-03-14 19:11:31 +0100

    a

--- extraer_fichero.c
+++ extraer_fichero.c
@@ -12,57 +12,143 @@
 struct s_Header fS;
 struct s_mypack_headers listC;
 
-int main(argc [], char* argv[]){
+int main(argc[], char *argv[])
+{
 
- if(argc < 2){
- fprintf(stderr, "Numero de argumentos invalido");
- exit(1);
- }
+    if (argc <= 2)
+    {
+        fprintf(stderr, "Numero de argumentos invalido");
+        exit(1);
+    }
 
+    int fichE = open(argv[1], O_RDONLY, 0600);
+    char buff[512]
 
- int fichE = open (argv[1],O_RDONLY,0600);
- char buff[512]
+        if (fichE = -1)
+    {
+        fprintf(stderr, "Fichero inexistente");
+        exit(2);
+    }
 
- if(fichE = -1){
- fprintf(stderr,"Fichero inexistente");
- exit(2);
- }
+    if (argc > 3)
+    {
+        fprintf(stderr, "Demasiados argumentos");
+        exit(3);
+    }
 
- if(argc > 3){
- fprintf(stderr,"Demasiados argumentos");
- exit(3);
- }
+    String texto;
+    int fichS = open(argv[2], O_CREAT, 0600);
+    texto = read(fichE, buff, 1);
 
- String texto;
- int fichS= open(argv[2],O_CREAT,0600);
- texto = read(fichE,buff,1);
+    while (read(fichE, buff, 1) > 0)
+    {
+        texto += buff;
+        if (strstr(texto, "Tipo: ") != NULL)
+        {
+            free(texto);
+            while (read(fichE, buff, 1) > 0)
+            {
+                if (buff != " ")
+                {
+                    fS.FileInfo.Type += buff;
+                }
+            }
+        }
+        if (strstr(texto, "Compresion: ") != NULL)
+        {
+            free(texto);
+            while (read(fichE, buff, 1) > 0)
+            {
+                if (buff != " ")
+                {
+                    fS.FileInfo.C += buff;
+                }
+            }
+        }
+        if (strstr(texto, "Tipo: ") != NULL)
+        {
+            free(texto);
+            while (read(fichE, buff, 1) > 0)
+            {
+                if (buff != " ")
+                {
+                    fS.FileInfo.Type += buff;
+                }
+            }
+        }
+        if (strstr(texto, "Compresion: ") != NULL)
+        {
+            free(texto);
+            while (read(fichE, buff, 1) > 0)
+            {
+                if (buff != " ")
+                {
+                    fS.FileInfo.Compress += buff;
+                }
+            }
+        }
+    }
+    if (strstr(texto, "Tamano: ") != NULL)
+    {
+        free(texto);
+        while (read(fichE, buff, 1) > 0)
+        {
+            if (buff != " ")
+            {
+                fS.FileInfo.DataSize += buff;
+            }
+        }
+    }
+    if (strstr(texto, "TamanoComp: ") != NULL)
+    {
+        free(texto);
+        while (read(fichE, buff, 1) > 0)
+        {
+            if (buff != " ")
+            {
+                fS.FileInfo.CompSize += buff;
+            }
+        }
+    }
+    if (strstr(texto, "Nombre: ") != NULL)
+    {
+        free(texto);
+        while (read(fichE, buff, 1) > 0)
+        {
+            if (buff != " ")
+            {
+                fS.FileInfo.DataFileName += buff;
+            }
+        }
+    }
+    if (strstr(texto, "DataPos: ") != NULL)
+    {
+        free(texto);
+        while (read(fichE, buff, 1) > 0)
+        {
+            if (buff != " ")
+            {
+                fS.FileInfo.DatPosition += buff;
+            }
+        }
+        listC[sizeof(listC)] = fS;
+    }
+    if (sizeof(listC) > argv[2])
+    {
+        write(fichS, "Tipo: ", strlen("+ Tipo: "));
+        write(fichS, &listC.vHead[argv[2]].FileInfo.Type, 1);
+        write(fichS, " Compresion: ", strlen(" Compresion: "));
+        write(fichS, &listC.vHead[argv[2]].FileInfo.Compress, 1);
+        write(fichS, " Tamano: ", strlen(" Tamano: "));
+        write(fichS, listC.vHead[argv[2]].FileInfo.DataSize, strlen(listC.vHead[argv[2]].FileInfo.DataSize));
+        write(fichS, " TamanoComp: ", strlen(" TamanoComp: "));
+        write(fichS, listC.vHead[argv[2]].FileInfo.Compsize, strlen(listC.vHead[argv[2]].FileInfo.DataSize));
+        write(fichS, " Nombre: ", strlen(" Nombre: "));
+        write(fichS, listC.vHead[argv[2]].FileInfo.DataFileName, strlen(listC.vHead[argv[2]].FileInfo.DataFileName));
+        write(fichS, " DataPos: ", strlen(" DataPos: "));
+        write(fichS, "0", strlen("0")); // ToDo: conseguir parametro con DataPos
+    }
 
- while(read(fichE,buff,1) > 0){
- texto += buff; 
-  if(strstr(texto,"Tipo: ") != NULL){
-  free(texto);
-  while(read(fichE,buff,1) > 0){
-   if(buff != " "){
-   fS.FileInfo.Type += buff;
-   }
-   if(buff != " "){
-   fS.FileInfo.Compress += buff;
-   }
-   if(buff != " "){
-    fS.FileInfo.DataSize += buff;
-   }
-   if(buff = " "){
-   fS.FileInfo.CompSize += buff; 
-   } 
-   if(buff = " " ){
-    fS.FileInfo.   
-   }
-   
-  }
- }
- }
-
-
-close(fichS);
-return(0);
+    close(fichS);
+    return (0);
 }
